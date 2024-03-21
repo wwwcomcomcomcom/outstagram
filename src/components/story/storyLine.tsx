1ff9138a5f08aa543fd12a33bcea78f85d80ed8a
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-03-21 19:02:38 +0900

    Made stories can be scrolled

--- src/components/story/storyLine.tsx
+++ src/components/story/storyLine.tsx
@@ -3,11 +3,11 @@ import Story from "./story.tsx";
 export default function StoryLine(){
     
     function getStories() {
-        return [
-            <Story/>,
-            <Story/>,
-            <Story/>
-        ]
+        const array = [];
+        for (let i = 0; i < 10; i++) {
+            array.push(<Story/>);
+        }
+        return array;
     }
     
     return <div className={styles.storyLine}>
