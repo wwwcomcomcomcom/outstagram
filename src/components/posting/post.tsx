1da46a6256665d2b99a2bff8ba8a00428874ccaf
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-03-24 00:44:28 +0900

    Add post component prototype

---
+++ src/components/posting/post.tsx
@@ -0,0 +1,7 @@
+import styles from "./post.module.css"
+
+export default function Post() {
+    return <div className={styles.post}>
+        {/*<img src="public/images/default-post.png" alt="Image Loading"/>*/}
+    </div>
+}
