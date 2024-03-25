0b342f8b6b2bf9cda093a66553f6d2890fa992b9
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-03-25 20:35:51 +0900

    Use PostScroll instead of Post

--- src/routes/homePage.tsx
+++ src/routes/homePage.tsx
@@ -1,11 +1,9 @@
 // import Story from "../components/story/story.tsx";
 import StoryLine from "../components/story/storyLine.tsx";
-import Post from "../components/posting/post.tsx";
+import PostScroll from "../components/posting/postScroll.tsx";
 export default function HomePage(){
     return <div className="main">
         <StoryLine/>
-        <Post/>
-        <Post/>
-        <Post/>
+        <PostScroll/>
     </div>
 }
