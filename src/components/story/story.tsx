9d3e3b305fa538076171fe9e4a20895a345c78ab
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-03-25 20:34:34 +0900

    fix 'public' directory warning

--- src/components/story/story.tsx
+++ src/components/story/story.tsx
@@ -2,6 +2,6 @@ import styles from "./story.module.css";
 
 export default function Story() {
     return <div className={styles.story}>
-        <img src="public/images/default-profile.png" alt="Profile Not Found"/>
+        <img src="/images/default-profile.png" alt="Profile Not Found"/>
     </div>
 }
