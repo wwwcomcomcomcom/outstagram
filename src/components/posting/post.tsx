4175640a23a0e9e587efc9c1601fb08856d3ebcf
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-03-26 20:49:42 +0900

    Update PostFootBar

--- src/components/posting/post.tsx
+++ src/components/posting/post.tsx
@@ -8,6 +8,10 @@ export default function Post() {
 
     // Upload Type Test Case
     const minutesAgo = 36;
+    const likesAmount = 126;
+    const userName = "nmixx_official";
+    const explanation = "str";
+
 
     return <div className={styles.post}>
         <div className={styles.title}>
@@ -15,20 +19,24 @@ export default function Post() {
                 <img src="/images/default-profile.png" alt="Profile Not Found"/>
             </div>
             <div className={styles.infoBox}>
-                <span className={styles.infoTitle}>nmixx_official • <span style={{fontWeight:400, fontSize:"0.8rem", color:"grey"}}>{minutesAgo}분전</span></span>
+                <span className={styles.infoTitle}>{userName} • <span style={{fontWeight:400, fontSize:"0.8rem", color:"grey"}}>{minutesAgo}분전</span></span>
             </div>
         </div>
         <div className={styles.image}></div>
         <div className={styles.postFootBar}>
             <div className={styles.transactionBar}>
-                <LikeButton className={styles.likesAmount}/>
-                <CommentButton/>
-                <ShareButton/>
-                <BookmarkButton/>
+                <LikeButton className={styles.likeButton}/>
+                <CommentButton className={styles.commentButton}/>
+                <ShareButton className={styles.shareButton}/>
+                <div className={styles.bookmarkButtonWrapper}>
+                    <BookmarkButton className={styles.bookmarkButton}/>
+                </div>
             </div>
-            <div className={styles.likesAmount}></div>
-            <div className={styles.bestComment}></div>
-            <div className={styles.showCommentButton}></div>
+            <div className={styles.likesAmount}>좋아요 {likesAmount}개</div>
+            <div className={styles.explanationBox}>
+                <span className={styles.userName}>{userName} </span>
+                <span className={styles.explanation}>{explanation}</span>
+            </div>
         </div>
         {/*<img src="public/images/default-post.png" alt="Image Loading"/>*/}
     </div>
