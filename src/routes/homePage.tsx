92eae2ec3a6c536c8130a3ecf6e1e79883f60472
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-04-26 12:29:18 +0900

    separate main layout from layout.css

--- src/routes/homePage.tsx
+++ src/routes/homePage.tsx
@@ -1,8 +1,10 @@
 // import Story from "../components/story/story.tsx";
 import StoryLine from "../components/story/storyLine.tsx";
 import PostScroll from "../components/posting/postScroll.tsx";
+import styles from "./home.module.css";
+
 export default function HomePage(){
-    return <div className="main">
+    return <div className={styles.main}>
         <StoryLine/>
         <PostScroll/>
     </div>
